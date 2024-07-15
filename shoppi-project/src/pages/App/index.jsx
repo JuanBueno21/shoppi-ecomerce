39de409d2902b325f0c250146bd62ab09385e40d
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-07-15 16:23:34 -0500

    global context

--- shoppi-project/src/pages/App/index.jsx
+++ shoppi-project/src/pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from "react-router-dom"
+import { ShoppingCardProvider } from "../../context"
 import Home from "../Home"
 import MyAccount from "../MyAccount"
 import MyOrder from "../MyOrder"
@@ -10,35 +11,24 @@ import './App.css'
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {
-      path: "/", element: <Home />
-    },
-    {
-      path: "/my-account", element: <MyAccount />
-    },
-    {
-      path: "/my-order", element: <MyOrder />
-    },
-    {
-      path: "/my-orders", element: <MyOrders />
-    },
-    {
-      path: "/*", element: <NotFound />
-    },
-    {
-      path: "/sign-in", element: <SignIn />
-    },
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/*", element: <NotFound /> },
+    { path: "/sign-in", element: <SignIn /> },
   ])
   return routes
 }
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-      <Navbar />
-    </BrowserRouter>
-
+    <ShoppingCardProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </ShoppingCardProvider>
   )
 }
 
