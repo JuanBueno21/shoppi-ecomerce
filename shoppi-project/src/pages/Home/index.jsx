e4b829c3a41181297516d71af9dd1fd9b0ada4e4
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-07-16 12:09:39 -0500

    product details

--- shoppi-project/src/pages/Home/index.jsx
+++ shoppi-project/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Layout from '../../components/layout'
 import Card from "../../components/Card"
+import ProductDetail from '../../components/productDetail'
 
 function Home() {
   const [items, setItems] = useState(null)
@@ -21,6 +22,7 @@ function Home() {
           })
         }
       </div>
+      <ProductDetail />
     </Layout>
   )
 }
