95139d0e00dd6db4208ad992611d4281b2610e42
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-07-17 14:57:32 -0500

    open product detail

--- shoppi-project/src/components/productDetail/index.jsx
+++ shoppi-project/src/components/productDetail/index.jsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCardContext } from "../../context"
 import "./style.css"
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCardContext)
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside 
+    className={`${context.isProductDetailOpen ? "flex" : "hidden"} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <div> 
+        <div>
           <XMarkIcon className="size-6 text-black">
 
           </XMarkIcon>
