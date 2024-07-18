2b3d429b942c081dc70e485b1fafd9ee34160d7e
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-07-18 17:10:52 -0500

    purchase order

--- shoppi-project/src/components/ordersCard/index.jsx
+++ shoppi-project/src/components/ordersCard/index.jsx
@@ -1,16 +1,22 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts } = props
-  
+
 
   return (
-    <div className="flex justify-between items-center mb-3 border-black">
-      <p>
-        <span>18.07.24</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
-      </p>
+    <div className="flex justify-between items-center border border-black rounded-lg p-4 w-80 mb-4">
+      <div className='flex w-full justify-between'>
+        <p className='flex flex-col'>
+          <span className='font-light'>18.07.24</span>
+          <span className='font-light'>Articles: {totalProducts}</span>
+        </p>
+        <p className='flex gap-2 items-center'>
+          <span className='font-medium text-3xl'>${totalPrice}</span>
+          <ChevronRightIcon
+            className="size-6 text-black cursor-pointer" />
+        </p>
+      </div>
     </div>
   )
 }
